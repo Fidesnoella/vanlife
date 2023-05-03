ea136e069f79d8c3f062df7fdce7352a21292878
Fidesnoella <fniragena@gmail.com>
2023-05-03 16:51:13 +0200

    feat: create home and about page

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="bg-[#FFF7ED] py-12 px-4">
+            <div className="max-w-7xl mx-auto container flex  items-center justify-between">
+                <Link to={"/"}>
+                    <h1 className="font-black text-[1.625rem] leading-10 cursor-pointer">#VANLIFE</h1>
+                </Link>
+                <ul className="flex gap-4 text-[#4D4D4D] text-base font-semibold leading-6 cursor-pointer">
+                    <Link to={"/about"}>
+                        <li>About</li>
+                    </Link>
+                    <li>Vans</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
